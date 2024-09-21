13f49ce6420444418162b0d5f9ef3dac46d1a519
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-09-21 16:01:58 -0300

    ♻️ Change `config.ini` to `.env`

--- backend.py
+++ backend.py
@@ -1,37 +1,15 @@
-#   _____  _                       _   ____        _     _______                   _       _
-#  |  __ \(_)                     | | |  _ \      | |   |__   __|                 | |     | |
-#  | |  | |_ ___  ___ ___  _ __ __| | | |_) | ___ | |_     | | ___ _ __ ___  _ __ | | __ _| |_ ___
-#  | |  | | / __|/ __/ _ \| '__/ _` | |  _ < / _ \| __|    | |/ _ \ '_ ` _ \| '_ \| |/ _` | __/ _ \
-#  | |__| | \__ \ (_| (_) | | | (_| | | |_) | (_) | |_     | |  __/ | | | | | |_) | | (_| | ||  __/
-#  |_____/|_|___/\___\___/|_|  \__,_| |____/ \___/ \__|    |_|\___|_| |_| |_| .__/|_|\__,_|\__\___|
-#                                                                           | |
-#                                                                           |_|
-#
-# Welcome to Raj's Discord Bot Template
-#
-# This template is designed to be a starting point for your own Discord bot.
-# Made by RajDave69 on GitHub.
-#
-#
-# This is the backend.py file. Here is where you'll find all the things which are run on initialization.
-# Here you'll find the client, the logger, and the config file's variables.
-# If you want to import a global variable or function to multiple cogs, put it here.
-# You can also add more variables to the config file and import them here.
-#
-#
-
-import configparser
 import sys
 import discord
 import logging
 from discord.ext import commands
 from colorlog import ColoredFormatter
+from dotenv import load_dotenv
+import os
 
 intents = discord.Intents.default()
 
-
 # Initializing the logger
-def colorlogger(name: str = 'my-discord-bot') -> logging.log:
+def colorlogger(name: str = 'my-discord-bot') -> logging.Logger:
     logger = logging.getLogger(name)
     stream = logging.StreamHandler()
 
@@ -39,35 +17,29 @@ def colorlogger(name: str = 'my-discord-bot') -> logging.log:
     logger.addHandler(stream)
     return logger  # Return the logger
 
-
 log = colorlogger()
 
-# Loading config.ini
-config = configparser.ConfigParser()
+# Load environment variables from .env file
+load_dotenv(dotenv_path='.env')
 
 try:
-    config.read('./data/config.ini')
-except Exception as e:
-    log.critical("Error reading the config.ini file. Error: " + str(e))
-    sys.exit()
+    # Getting the variables
+    log_level = os.getenv('LOG_LEVEL', 'INFO')
+    presence = os.getenv('PRESENCE', 'Bot is running!')
 
-# Getting variables from config.ini
-try:
-    # Getting the variables from `[general]`
-    log_level: str = config.get('general', 'log_level')
-    presence: str = config.get('general', 'presence')
+    discord_token = os.getenv('DISCORD_TOKEN')
+    if discord_token is None:
+        log.critical("DISCORD_TOKEN is not set in the .env file.")
+        sys.exit()
 
-    # Getting the variables from `[secret]`
-    discord_token: str = config.get('secret', 'discord_token')
+    embed_footer = os.getenv('EMBED_FOOTER', '')
+    embed_color_str = os.getenv('EMBED_COLOR', '0x1a1aff')
+    embed_url = os.getenv('EMBED_URL', '')
 
-    # Getting the variables from `[discord]`
-    embed_footer: str = config.get('discord', 'embed_footer')
-    embed_color: int = int(config.get('discord', 'embed_color'), base=16)
-    embed_url: str = config.get('discord', 'embed_url')
-
-
+    # Convert embed_color to int
+    embed_color = int(embed_color_str, base=16)
 except Exception as err:
-    log.critical("Error getting variables from the config file. Error: " + str(err))
+    log.critical("Error getting variables from the .env file. Error: " + str(err))
     sys.exit()
 
 # Set the logger's log level to the one in the config file
@@ -78,17 +50,7 @@ else:
     log.warning(f"Invalid log level `{log_level.upper().strip()}`. Defaulting to INFO.")
 
 # Initializing the client
-client = commands.Bot(intents=intents)  # Setting prefix
-
-_embed_template = discord.Embed(
-    title="Error!",
-    color=embed_color,
-    url=embed_url
-)
-_embed_template.set_footer(text=embed_footer)
-
-embed_template = lambda: _embed_template.copy()
-
+client = commands.Bot(intents=intents)
 
 def error_template(description: str) -> discord.Embed:
     _error_template = discord.Embed(
