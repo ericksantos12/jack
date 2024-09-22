a4b1efdffea7b4f75ac898e0dc5fc529f4804cd7
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-09-22 10:44:46 -0300

    💄 Make owner commands ephemeral

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord.commands import Option, permissions
+from discord.commands import Option
 from backend import log, error_template
 
 
@@ -9,8 +9,8 @@ class Admin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        log.info("Cog: Admin.py Loaded")
-
+        log.info("Cog: admin.py Loaded")
+        
     # Load
     @commands.slash_command(name="load", description="Load a cog")
     @commands.is_owner()
@@ -18,10 +18,10 @@ class Admin(commands.Cog):
         try:
             self.bot.load_extension(f"cogs.{extension}")
             log.info(f"Loaded {extension}")
-            await ctx.respond(f"✅ Loaded {extension}")
+            await ctx.respond(f"✅ Loaded {extension}", ephemeral=True)
         except Exception as e:
             log.error(e)
-            await ctx.respond(embed=error_template(e))
+            await ctx.respond(embed=error_template(e), ephemeral=True)
         
     # Unload
     @commands.slash_command(name="unload", description="Unload a cog")
@@ -30,10 +30,10 @@ class Admin(commands.Cog):
         try:
             self.bot.unload_extension(f"cogs.{extension}")
             log.info(f"Unloaded {extension}")
-            await ctx.respond(f"🔥 Unloaded {extension}")
+            await ctx.respond(f"🔥 Unloaded {extension}", ephemeral=True)
         except Exception as e:
             log.error(e)
-            await ctx.respond(embed=error_template(e))
+            await ctx.respond(embed=error_template(e), ephemeral=True)
         
     # Reload
     @commands.slash_command(name="reload", description="Reload a cog")
@@ -42,10 +42,10 @@ class Admin(commands.Cog):
         try:
             self.bot.reload_extension(f"cogs.{extension}")
             log.info(f"Reloaded {extension}")
-            await ctx.respond(f"🔃 Reloaded {extension}")
+            await ctx.respond(f"🔃 Reloaded {extension}", ephemeral=True)
         except Exception as e:
             log.error(e)
-            await ctx.respond(embed=error_template(e))
+            await ctx.respond(embed=error_template(e), ephemeral=True)
     
     # Sync
     @commands.slash_command(name="sync", description="Sincronizar todos os comandos de barra")
@@ -54,10 +54,10 @@ class Admin(commands.Cog):
         try:
             await self.bot.sync_commands()
             log.info(f"Synced commands globally.")
-            await ctx.respond(f"📡 Synced commands globally.")
+            await ctx.respond(f"📡 Synced commands globally.", ephemeral=True)
         except Exception as e:
             log.error(e)
-            await ctx.respond(embed=error_template(e))
+            await ctx.respond(embed=error_template(e), ephemeral=True)
 
 def setup(client):
     client.add_cog(Admin(client))
